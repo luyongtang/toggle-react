7a80d9f47b9af90ca46bff9de8479056e2c595ce
Yongtang Lu <wilsonluyongtang_2005@hotmail.com>
2019-08-31 20:14:01 -0400

    Added a simple component 'ToggleSegment' to wrap a toggle in a segment

--- src/component/togglePage.jsx
+++ src/component/togglePage.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Toggle from "./common/toggle";
+import { Segment } from "semantic-ui-react";
+import ToggleSegment from "./common/toggleSegment";
 
 class TogglePage extends Component {
   render() {
     return (
       <div>
         <h1>Feature Toggle Configuration</h1>
-        <Toggle id="switch" label="hello" />
+        <ToggleSegment />
       </div>
     );
   }
